aea49be0cd8915908dec70109534eedeb5c9d7c1
Wender Mrn <wender.mrn@gmail.com>
2017-05-20 00:53:26 -0300

    Resolvendo 2º simulado de Ruby

---
+++ simulado-ruby2/cine-bangue/cine.rb
@@ -0,0 +1,43 @@
+require 'json'
+require 'net/http'
+
+class Cine
+
+  def initialize
+    @movies = JSON.parse(File.read('filmes.json'))
+    @temps = JSON.parse(File.read('temporada.json'))
+  end
+
+  def list_times(movie_name)
+    result  = []
+    @movies.each{|id,movie|
+      if movie["nome"].upcase == movie_name.upcase
+        movie_key = id
+        @temps[@temps.keys[0]]["programacao"].each{|data_prog,times|
+          if times.has_value?(movie_key)
+            result.push({date: data_prog,time: times.select{|key,value| value == movie_key }.keys.first})
+          end
+        }
+        break
+      end
+    }
+    result
+  end
+
+  def list_movies(data)
+    result  = []
+    puts "LIST_MOVIES: #{@temps.keys[0]}"
+    @temps[@temps.keys[0]]["programacao"].each{|data_prog,times|
+      if data_prog == data
+        movie_keys = times.values
+          @movies.reverse_each{|id,movie|
+            if movie_keys.include?(id)
+                result.push({movie: movie["nome"],time: times.select{|key,value| value == id }.keys.first})
+            end
+          }
+        break
+      end
+    }
+    result
+  end
+end
